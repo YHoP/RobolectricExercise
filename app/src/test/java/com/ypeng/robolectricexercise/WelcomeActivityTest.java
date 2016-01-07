b5a5659b8c59d266d201769af4fda64bb61bd2cd
YHop <ypeng.tw@gmail.com>
2016-01-06 18:16:12 -0800

    Add setup and clickingLogin_shouldStartLoginActivity test

---
+++ app/src/test/java/com/ypeng/robolectricexercise/WelcomeActivityTest.java
@@ -0,0 +1,35 @@
+package com.ypeng.robolectricexercise;
+
+
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@RunWith(RobolectricTestRunner.class)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk=21)
+public class WelcomeActivityTest {
+
+    private WelcomeActivity mActivity;
+
+    @Before
+    public void setup() throws Exception {
+        mActivity = Robolectric.setupActivity(WelcomeActivity.class);
+    }
+
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() throws Exception {
+        mActivity.findViewById(R.id.login).performClick();
+
+        Intent expectedIntent = new Intent(mActivity, LoginActivity.class);
+        assertThat(Shadows.shadowOf(mActivity).getNextStartedActivity()).isEqualTo(expectedIntent);
+    }
+}
