25c2e16ed0895af68d3a6a497482d76819766288
YHop <ypeng.tw@gmail.com>
2016-01-06 18:14:12 -0800

    Add button setOnClick function

--- app/src/main/java/com/ypeng/robolectricexercise/WelcomeActivity.java
+++ app/src/main/java/com/ypeng/robolectricexercise/WelcomeActivity.java
@@ -1,13 +1,23 @@
 package com.ypeng.robolectricexercise;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final View button = findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
